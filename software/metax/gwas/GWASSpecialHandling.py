253198f934a03b4a79bf7763187ada57f8cdb01c
Tai-Wei Chiang <chiangtw112358@gmail.com>
2025-06-24 12:36:40 +0800

    modify some deprecated usage

--- software/metax/gwas/GWASSpecialHandling.py
+++ software/metax/gwas/GWASSpecialHandling.py
@@ -64,7 +64,10 @@ def gwas_data_source(path, snps=None, snp_column_name=None, skip_until_header=No
                 s[comp].append(c)
 
         for c in header_comps:
-            s[c] = numpy.array(pandas.to_numeric(s[c], errors='ignore'))
+            try:
+                s[c] = numpy.array(pandas.to_numeric(s[c]))
+            except:
+                s[c] = numpy.array(s[c])
 
     return s
 
